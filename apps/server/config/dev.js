a01ca8fe99a242ff7f002797c4e606a73a121154
romejiang <romejiang@gmail.com>
2023-12-21 03:01:06 +0800

    perf: 删除一些配置

--- apps/server/config/dev.js
+++ apps/server/config/dev.js
@@ -5,28 +5,27 @@ const config = {
     host: 'http://localhost:3000'
   },
   jwt: {
-    secret: 'fengkuang365'
+    secret: 'dev'
   },
   mongodb: {
     host: 'mongodb://localhost',
     db: 'shorttv'
   },
   s3: {
-    accountid: '9c5aee4853df53354e5b6c4459a99042',
-    key: '32492f0f20236047d19f58e1ed5da466',
-    secret: '9669eeaf084029230c11c13fe061dfe6b8a9558280a2b914763189de57ecd436',
-    bucket: 'shorttv',
-    endpoint:
-      'https://9c5aee4853df53354e5b6c4459a99042.r2.cloudflarestorage.com',
-    token: 'FklcI9RH-PNz-XCJhLZam7buykcgAghF698GxxFg',
-    site: 'https://tv.ai2app.xyz/'
+    accountid: '',
+    key: '',
+    secret: '',
+    bucket: '',
+    endpoint: '',
+    token: '',
+    site: ''
   },
   mp: {
-    appId: 'wx6bdb871fd12a1dc0',
-    appSecret: '38c830960ff466a7372a0c44572a1570'
+    appId: '',
+    appSecret: ''
   },
   redis: {
-    url: 'redis://:FcyUp202166c90496@59.110.229.203:6379/10'
+    url: ''
   }
 }
 
--- apps/server/readme.md
+++ apps/server/readme.md
@@ -1,22 +1,17 @@
 ## ShortTV
 
 ### TODO
-* 先实现前端需要的一些接口，让能跑起来。
-* 整合api接口
-
-* 支付
 * 分享
 * 返回随机短视频
 * 个人信息，头像，钱包，支付记录
 * 接入真实的评论
 * web 添加评论,完善评论接入后台
 
-* google 支付
-
 ### DEV
 
 
 ### 需要正式环境的才能测试的内容
+* google 支付
 * OAuth Apple 
 * OAuth facebook 认证，需要正式版才能测试，需要有应用市场的一些信息才行
 * google 统计，需要打包正式版才能有数据
@@ -40,36 +35,4 @@
 * 管理端和api端
   
 
-### PASS
 
-## 导出 mongo docker
-
-```shell
-docker run \
---rm \
--v $PWD/backup:/backup \
---network=mongodb-express_default \
-mongo \
-bash -c "mongodump --out /backup --host single-mongo -d $1"
-```
-
-
-## 导入宝塔 mongo
-
-```shell
-mongorestore -d shorttv --drop mongodb://shorttv:L5RM2Sr42YECNMyP@localhost:27017/shorttv ./shorttv
-
-```
-
-
-ShortTV 是全球最好的短剧APP全平台解决方案，支持IOS，Android，微信小程序，H5，抖音小程序
-
-APP 演示地址，账号可以自行注册
-http://h5.ai2app.xyz
-
-
-管理后台地址
-http://admin.ai2app.xyz
-
-超级管理员账号
-romejiang/1qazxsw2
